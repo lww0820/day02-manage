b2d900cde1913ca01109bf0ac3dc07073364f74a
“lww0820” <468456671@qq.com>
2023-07-26 20:48:30 +0800

    第二次提交

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      
+    };
+  },
+  methods: {},
+  computed: {},
+  components: {},
+  filters: {},
+  watch: {},
+  created() {},
+  mounted() {},
+};
 </script>
+
+<style lang="scss" scoped>
+  
+</style>
